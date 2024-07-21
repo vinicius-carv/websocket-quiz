abdc323325950fd2f7451510e159b6e3df05530a
vinicius-carv <valves8956@gmail.com>
2024-07-21 17:47:10 -0300

    update src

---
+++ client/src/App.jsx
@@ -0,0 +1,34 @@
+import React, {useEffect, useState} from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './pages/Home';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
+import './scss/App.scss';
+import Login from "./components/logon/Login.jsx";
+
+function App() {
+    const [isDarkMode, setIsDarkMode] = useState(
+        localStorage.getItem('theme') === 'dark'
+    );
+
+    useEffect(() => {
+        document.documentElement.setAttribute('data-theme', isDarkMode ? 'dark' : 'light');
+        localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
+    }, [isDarkMode]);
+    return (
+        <Router>
+            <Header />
+            <main>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/services" element={<Services />} />
+                    <Route path="/contact" element={<Contact />} />
+                </Routes>
+            </main>
+        </Router>
+    );
+}
+
+export default App
